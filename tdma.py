6ccd700ce518928a47eb1101c833a8c79c78fb5b
Alexandre Paes <alexandre_slp@poli.ufrj.br>
2017-06-15 20:26:15 -0300

    primeiro commit

---
+++ tdma.py
@@ -0,0 +1,11 @@
+# TriDiagonal Matrix Algorithm (TDMA) or Thomas Algorithm
+# Caso particular do algoritmo de eleiminacao Gaussiana
+# Algoritmo para resolver sistema de equacoes de primeira ordem
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def tdma_solver(tdm):
+    solved_tdm = 1
+    return solved_tdm
